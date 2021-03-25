94053259aeabffa856e2a62b9712fcc06442b82d
nashiox <u.4.o.holly12@gmail.com>
2021-03-25 15:53:02 +0900

    fix json type

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/nashiox/github-billing-exporter/cmd/github-billing-exporter/cmd"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	rootCmd := cmd.GetRootCmd(os.Args[1:])
 
 	if err := rootCmd.Execute(); err != nil {
